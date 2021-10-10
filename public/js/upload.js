8868972441507f17f0782c36c684dc8a71396c91
Ellie <ytaghavi@yahoo.com>
2021-10-09 18:49:32 -0700

    upload.ejs Uploads different File Formats

--- public/js/upload.js
+++ public/js/upload.js
@@ -2,22 +2,44 @@ const SelectForm = document.querySelector("#selectFormatForm");
 let UploadForm = document.querySelector("#UploadForm");
 let items = document.querySelectorAll(".format");
 
+
+let label = document.createElement("label");
+let button = document.createElement("button");
+let input = document.createElement("input");
+
+label.htmlFor = "data";
+input.id = "data";
+button.innerHTML = "Summarize it!";
+
 SelectForm.addEventListener("submit", function(e){
+    let choice;
     for(let i in items)
     {
-        let choice;
         if(items[i].checked)
         {choice = items[i].id}
     }
 
-    document.createElement
+    if(choice == "mp4" || choice == "mp3")
+    {
+        label.innerText = "Select a file: ";
+        input.type = "file";
+    }
+    else{
+        label.innerText = "Enter your link: ";
+        input.type = "text";
+    }
+
+    UploadForm.appendChild(label);
+    UploadForm.appendChild(input);
+    UploadForm.appendChild(button);
+
     e.preventDefault();
 })
 
 
 /*
 <form id="UploadForm">
-    <label for="file">Select a file:</label>
-    <input type="file" id="file" name="file" value="file">
+    <label for="data">Select a file:</label>
+    <input type="file" id="data" name="file" value="file">
     <input type="submit" value="Summarize it!">
 </form> */
